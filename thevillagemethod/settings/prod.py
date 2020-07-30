fc41645ff7addfc008f907e17567910b2644ecda
Ryan <ryanbeckwith2000@gmail.com>
2020-07-30 16:41:26 -0400

    Changed prod database settings to include posgresql for ENGINE

--- thevillagemethod/settings/prod.py
+++ thevillagemethod/settings/prod.py
@@ -31,5 +31,7 @@ DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
